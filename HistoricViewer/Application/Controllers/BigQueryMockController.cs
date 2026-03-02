f8826a1788a5f2e23a26f15d23e1097c30fd62fe
lin.pengpeng <lin.pengpeng@mayo.edu>
2026-03-02 09:36:35 -0600

    Add BigQueryMockController for mock testing endpoint

---
+++ HistoricViewer/Application/Controllers/BigQueryMockController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HistoricViewer.Application.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class BigQueryMockController: ControllerBase
+{
+    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var result = new[] { "bigquery", "mock testing" };
+        return Ok(result);
+    }
+    
+}
