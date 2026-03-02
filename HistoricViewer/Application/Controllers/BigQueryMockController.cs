9b1cc0432f8b8a1accb01a44ba36fdc463cfc9d6
lin.pengpeng <lin.pengpeng@mayo.edu>
2026-03-02 10:35:45 -0600

    Restore BigQueryMockController implementation for mock testing endpoint

--- HistoricViewer/Application/Controllers/BigQueryMockController.cs
+++ HistoricViewer/Application/Controllers/BigQueryMockController.cs
@@ -1,18 +1,18 @@
-// using Microsoft.AspNetCore.Mvc;
-//
-// namespace HistoricViewer.Application.Controllers;
-//
-//
-// [ApiController]
-// [Route("[controller]")]
-// public class BigQueryMockController: ControllerBase
-// {
-//     
-//     [HttpGet]
-//     public IActionResult Get()
-//     {
-//         var result = new[] { "bigquery", "mock testing" };
-//         return Ok(result);
-//     }
-//     
-// }
+using Microsoft.AspNetCore.Mvc;
+
+namespace HistoricViewer.Application.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class BigQueryMockController: ControllerBase
+{
+    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var result = new[] { "bigquery", "mock testing" };
+        return Ok(result);
+    }
+    
+}
